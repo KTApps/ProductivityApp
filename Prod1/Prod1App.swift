7d6b6455d28b11e5032a54b3e99d7186a384a7bd
Tanaka Bere <tanakabere@hotmail.co.uk>
2024-02-10 23:01:50 +0000

    Initial Commit

---
+++ Prod1/Prod1App.swift
@@ -0,0 +1,17 @@
+//
+//  Prod1App.swift
+//  Prod1
+//
+//  Created by Tanaka Bere on 10/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Prod1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
