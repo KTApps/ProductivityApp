36b2ea9750fed8706ad7962d6e506a45df92a951
C23097798 <mahajak@cardiff.ac.uk>
2024-02-11 23:47:29 +0000

    Added a '.zIndex(1)' to the drop down so that it will always have the greatest 'z coordinates' and appear at the front.

--- Prod1/ContentView.swift
+++ Prod1/ContentView.swift
@@ -85,7 +85,8 @@ struct ContentView: View {
                     }
                 }
             }
-            .padding()
+            .zIndex(1)
+            .padding(30)
             VStack {
                 VStack{
                     VStack{
@@ -158,9 +159,8 @@ struct ContentView: View {
 
         
         
-        struct ContentView_Previews: PreviewProvider {
-            static var previews: some View {
-                ContentView()
-            }
-        }
-// Testing Branch
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
