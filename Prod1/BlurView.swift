57844d464e247850ea7e9ba95dd7610e2ab8236f
C23097798 <mahajak@cardiff.ac.uk>
2024-02-16 17:40:47 +0000

    blurView Kinda complete

---
+++ Prod1/BlurView.swift
@@ -0,0 +1,18 @@
+//
+//  BlurView.swift
+//  Prod1
+//
+//  Created by Kelvin Mahaja on 16/02/2024.
+//
+
+import SwiftUI
+
+struct BlurView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlurView()
+}
---
+++ Prod1/HabitTracker.swift
@@ -0,0 +1,18 @@
+//
+//  HabitTracker.swift
+//  Prod1
+//
+//  Created by Kelvin Mahaja on 16/02/2024.
+//
+
+import SwiftUI
+
+struct HabitTracker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HabitTracker()
+}
