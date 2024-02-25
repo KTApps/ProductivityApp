eeac48fd54cb8f1545d18219c8a65b95097d2525
C23097798 <mahajak@cardiff.ac.uk>
2024-02-25 00:04:53 +0000

    Seriously smashed this session. Probably the best one yet. Created a file for our objects. Commented on everything to make code more readable. Fixed the issue of 2 array elements with the same value being connected. Fixed the issue of each array being copied to different weekdays. Aslo learnt a lot aswell such as classes & objects on swiftUI. go again next time!!!

---
+++ Prod1/Objects.swift
@@ -0,0 +1,22 @@
+//
+//  Objects.swift
+//  Prod1
+//
+//  Created by Kelvin Mahaja on 21/02/2024.
+//
+
+import SwiftUI
+
+class Objects: ObservableObject {
+    @Published var selectedHabit: String? // String that's about to be removed. '?' shows that it could be 'nil'
+    
+    @Published var habitIdArray: [String] = []
+    @Published var habitIdDict: [String: String] = [:] // Dictionary = [Habit Id : Habit Name]
+    @Published var habitDict: [String: String] = [:] // Dictionary = [Habit Id : Habit Time]
+    @Published var habitPositions: [String: CGSize] = [:] // Dictionary = [Habit Id : Habit coordinates]
+    @Published var isHabitStriked: [String: Bool] = [:] // Dictionary = [Habit Id : Striked/ Not Striked]
+    
+    @Published var WeekDayIndexCounter: Int = 0
+    @Published var WeekDay: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    @Published var WeekDayHabits: [String: [String]] = [:] // Dictionary = [Weekday : Array of Habit Id's]
+}
