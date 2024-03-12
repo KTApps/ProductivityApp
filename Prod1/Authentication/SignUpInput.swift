ca07f96d987d5ec5a9bbdd7db6676ed3da8c8521
C23097798 <mahajak@cardiff.ac.uk>
2024-03-12 00:08:33 +0000

    Authentication views done. Now just need to link to the firebase

---
+++ Prod1/Authentication/SignUpInput.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpInput.swift
+//  Prod1
+//
+//  Created by Kelvin Mahaja on 11/03/2024.
+//
+
+import SwiftUI
+
+struct SignUpInput: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpInput()
+}
