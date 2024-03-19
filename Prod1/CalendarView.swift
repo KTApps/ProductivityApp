c556cfeb71d13951eda397c3df15b4e250498b20
C23097798 <mahajak@cardiff.ac.uk>
2024-03-19 19:32:39 +0000

    Started the Progress Bar. Its going well so far!!

---
+++ Prod1/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  Prod1
+//
+//  Created by Kelvin Mahaja on 18/03/2024.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
---
+++ Prod1/ManualCalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  ManualCalendarView.swift
+//  Prod1
+//
+//  Created by Kelvin Mahaja on 19/03/2024.
+//
+
+import SwiftUI
+
+struct ManualCalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ManualCalendarView()
+}
